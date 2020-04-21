11b6c019b59c28f9d2f03d33dd6507e47997e1e5
Bobby Neal <rsneal01@gmail.com>
2020-04-21 17:17:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU GRANDMA"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != phrase.upcase
+  # if phrase = "I LOVE YOU GRANDMA"
+  #   return "I LOVE YOU TOO PUMPKIN!"
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end

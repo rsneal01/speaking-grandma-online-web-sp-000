b05a9508770aa52b5b2e0f9aa71a07cd959c5886
Bobby Neal <rsneal01@gmail.com>
2020-04-21 17:20:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(phrase)
   #   return "I LOVE YOU TOO PUMPKIN!"
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
   end

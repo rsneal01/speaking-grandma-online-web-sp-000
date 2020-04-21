a74462c1907c4c0aad21cfb8f7f3f240e8a1c6c6
Bobby Neal <rsneal01@gmail.com>
2020-04-21 17:05:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,15 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
+
+
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
